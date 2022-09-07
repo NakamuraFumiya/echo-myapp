6c210c4eca81f7ced767babb53f7c240509ef1ad
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2022-09-07 09:34:07 +0900

    コメント追加

--- server.go
+++ server.go
@@ -7,9 +7,14 @@ import (
 )
 
 func main() {
+	// Echo instance
 	e := echo.New()
+
+	// Routes
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	// Start Server
 	e.Logger.Fatal(e.Start(":1323"))
 }
