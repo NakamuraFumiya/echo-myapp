ab2155ed263c536dec55cf095cd354eec84a74a7
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2022-09-10 13:03:20 +0900

    ログ出力するようにmiddlewareを追加

--- server.go
+++ server.go
@@ -4,11 +4,16 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	// Echo instance
 	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	// Routes
 	e.GET("/", hello)
