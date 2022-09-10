c5c463c53e06064219c1f44838a75cefebd46882
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2022-09-10 14:38:07 +0900

    userを擬似的に生成するように見せかけるエンドポイントを追加

--- server.go
+++ server.go
@@ -20,6 +20,7 @@ func main() {
 	// e.POST("/users", saveUser)
 	e.GET("/users/:id", getUser)
 	e.GET("/show", show)
+	e.POST("/save", save)
 
 	// Start Server
 	e.Logger.Fatal(e.Start(":1323"))
@@ -43,4 +44,12 @@ func show(c echo.Context) error {
 	team := c.QueryParam("team")
 	member := c.QueryParam("member")
 	return c.String(http.StatusOK, "team:"+team+", member:"+member)
+}
+
+// e.POST("/users", saveUser)
+func save(c echo.Context) error {
+	// Get name and email
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name:"+name+", email:"+email)
 }
